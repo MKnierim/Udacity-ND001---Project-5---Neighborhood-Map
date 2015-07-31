a8fc5551386754f40f3c6d6e35c6f3a5e202c403
Michael Knierim <kontakt@michaelknierim.de>
2015-07-31 09:09:01 +0200

    Finish marker activation highlighting

--- js/model.js
+++ js/model.js
@@ -1,9 +1,13 @@
 // Code for the apps data model (Part of knockout.js MVVM pattern for the separation of concerns)
 var model = {
-	markerArray: ko.observableArray()
+	markerArray: ko.observableArray(),		// Observable Array to store all marker objects and automatically update the view on changes.
+
+	activeMarker: null,		// Stores an object of type marker which is currently being focused.
+
+	activeInfo: null		// Stores an object of type infoWindo which belongs to the marker which is currently being focused.
 };
 
-// Create a marker object as a decorator for the Google Maps marker object. This way aditional attributes of a marker can be specified.
+// Create a marker object as a child of the Google Maps marker object. This way aditional attributes of a marker can be specified.
 // var Marker = function () {
 // 	this.gmObject = function(location, map, title) {
 // 		var gmMarker = new google.maps.Marker({
--- js/viewmodel.js
+++ js/viewmodel.js
@@ -4,6 +4,20 @@
 var map;
 var karlsruhe = new google.maps.LatLng(49.006616, 8.403354);
 
+var labelChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+var labelIndex = 0;
+
+// Specifies a custom designed icon for the map. Maybe I could nest this in a newly created Marker object (a child of the GoogleMapsMarker)?
+var greenIcon = {
+	url: 'imgs/icons/marker-green.png',
+	labelOrigin: new google.maps.Point(12, 12)
+};
+
+var orangeIcon = {
+	url: 'imgs/icons/marker-orange.png',
+	labelOrigin: new google.maps.Point(12, 12)
+};
+
 var viewModel = {
 
 	initializeMap: function() {
@@ -33,21 +47,11 @@ var viewModel = {
 	},
 
 	addMarker: function(location, map, title) {
-		// Specifies a custom designed icon for the map.
-		var greenIcon = {
-			url: 'imgs/icons/marker-green.png',
-			labelOrigin: new google.maps.Point(12, 12)
-		};
+		var labelChar = labelChars[labelIndex++ % labelChars.length];
 
-		var orangeIcon = {
-			url: 'imgs/icons/marker-orange.png',
-			labelOrigin: new google.maps.Point(12, 12)
-		};
-
-		// Specifies a label designed to match the custom icon.
 		var customLabel = {
 			color: '#fff',
-			text: String(model.markerArray().length+1),
+			text: labelChar,
 			fontWeight: 'bold'
 		};
 
@@ -55,11 +59,11 @@ var viewModel = {
 		var marker = new google.maps.Marker({
 			position: location,
 			map: map,
-			label: customLabel,
 			icon: greenIcon,
+			label: customLabel,
 			// cursor: 'pointer', did not seem to work like this
 			draggable: true,
-			title: "#" + (model.markerArray().length+1) + " - " + title
+			title: "#" + labelChar + " - " + title
 		});
 
 		// Creates an info window for the marker
@@ -69,12 +73,32 @@ var viewModel = {
 
 		// This event listener opens the info window on the marker when it is clicked.
 		google.maps.event.addListener(marker, 'click', function() {
-			marker.setIcon(orangeIcon);		// Change appearance of marker to show activation
-			infoWindow.open(map,marker);
+			viewModel.updateActiveMarker(marker, infoWindow);
 		});
 
-	// Add marker to observable array in model object.
-	model.markerArray.push(marker);
+		// This event listener sets the marker to be displayed as inacive when the info window is closed manually
+		google.maps.event.addListener(infoWindow, 'closeclick', function() {
+			marker.setIcon(greenIcon);
+		});
+
+		// Add marker to observable array in model object and activate new marker.
+		model.markerArray.push(marker);
+		this.updateActiveMarker(marker, infoWindow);
+	},
+
+	updateActiveMarker: function(marker, infoWindow) {
+		if (model.activeMarker != null && model.activeMarker != marker) {
+			// If there is actually a new marker that should be activated change icon and close info window on a previously active marker.
+			model.activeInfo.close();
+			// infoWindow.close(map, model.activeMarker);
+			model.activeMarker.setIcon(greenIcon);
+		};
+
+		// Set and activate the new marker
+		model.activeMarker = marker;
+		model.activeInfo = infoWindow;
+		model.activeMarker.setIcon(orangeIcon);
+		infoWindow.open(map,model.activeMarker);
 	}
 };
 
